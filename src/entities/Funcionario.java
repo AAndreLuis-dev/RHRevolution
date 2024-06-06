4088d91c741e917d85af46d6cf4f50d3c6f16a6e
Mallock <mallock.s16@gmail.com>
2024-06-05 22:50:31 -0300

    Add comment to Funcionario

--- src/entities/Funcionario.java
+++ src/entities/Funcionario.java
@@ -2,6 +2,7 @@ package entities;
 
 public class Funcionario {
 
+    // Atributos privados e protegidos da classe Funcionario
     private int id = 0;
     protected String nome;
     protected String email;
@@ -9,17 +10,21 @@ public class Funcionario {
     protected String cpf;
     protected int mesesTrabalho;
 
-    static int proximoId;
+    static int proximoId; // Contador estático para gerar IDs únicos para os funcionários
 
+    // Construtor que inicializa um Funcionario com nome, email, salário e CPF
     public Funcionario(String nome, String email, double salario, String cpf) {
-        this.id = proximoId;
-        proximoId++;
+        this.id = proximoId; // Atribui o valor de proximoId ao id do funcionário
+        proximoId++; // Incrementa proximoId para o próximo funcionário
         this.nome = nome;
         this.email = email;
         this.salario = salario;
         this.cpf = cpf;
-
     }
+
+
+    // Métodos getter e setter para acessar e modificar os atributos do funcionário
+
 
     public Funcionario(String nome, String email, double salario, String cpf, int mesesTrabalho) {
         this.id = proximoId;
@@ -58,12 +63,15 @@ public class Funcionario {
 
     public void setMesesTrabalho(int mesesTrabalho) {this.mesesTrabalho = mesesTrabalho;}
 
+    // Método toString que retorna uma string formatada com o nome, email e CPF do funcionário
     public String toString(int numero) {
         return String.format("\nNome: %s"
                 + "\nemail: %s"
                 + "\nCPF: %s", this.nome, this.email, this.cpf);
     }
 
+    // Método toString que retorna uma string formatada com todos os atributos do funcionário
+    @Override
     public String toString() {
         return String.format("\nID: %d"
                 + "\nNome: %s"
